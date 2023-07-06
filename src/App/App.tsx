19f1659af4d3c6ffde9e420106c825f17d709089
h12t20 <sergdiamond@ymail.com>
2023-07-06 14:37:35 +0300

    refactoring reducer

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,103 +4,9 @@ import {Counter} from "../Counter/Counter";
 import {Set} from "../Set/Set";
 import {Logo} from '../Logo/Logo';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-
-export type StateType = {
-    value: number
-    minValue: number
-    maxValue: number
-    error: string
-    inputMinTitle: number
-    inputMaxTitle: number
-}
-
-export const reducer = (state: StateType, action: ReducerType): StateType => {
-    switch (action.type) {
-        case 'RESET': {
-            return {
-                ...state,
-                value: state.minValue ? state.minValue : 0,
-                inputMinTitle: state.minValue ? state.minValue : 0,
-                inputMaxTitle: state.maxValue ? state.maxValue : 10,
-                error: ''
-            }
-        }
-        case 'MIN_TITLE': {
-            return {
-                ...state,
-                inputMinTitle: +action.payload.value <= state.inputMaxTitle &&
-                +action.payload.value >= -1 ?
-                    +action.payload.value : state.inputMinTitle,
-                error: +action.payload.value >= state.inputMaxTitle ? 'Err1' :
-                    +action.payload.value < 0 ? 'Err2' : state.value >= state.maxValue ? state.value.toString():''
-            }
-        }
-        case 'MAX_TITLE': {
-            return {
-                ...state,
-                inputMaxTitle: +action.payload.value >= state.inputMinTitle &&
-                +action.payload.value >= 0 ?
-                    +action.payload.value : state.inputMaxTitle,
-                error: +action.payload.value <= state.inputMinTitle ? 'Err1' :
-                    state.value >= state.maxValue ? state.value.toString() : ''
-            }
-        }
-        case 'SET_HANDLER': {
-            return state.error.slice(0,2)!=='Er'? {
-                ...state,
-                minValue: state.inputMinTitle,
-                maxValue: state.inputMaxTitle,
-                value: state.inputMinTitle,
-                error:''
-            }: state
-        }
-        case 'INC_HANDLER': {
-            return state.error.slice(0,2)!=='Er'? {
-                ...state,
-                value: state.value < state.maxValue ? state.value + 1 : state.maxValue,
-                error: !state.error && state.value >= state.maxValue - 1 ? state.value.toString() : state.error
-            } : state
-        }
-        default:
-            return state
-    }
-}
-type ReducerType = ResetACType | MinTitleChangeACType | MaxTitleChangeACType | SetHandlerACType | IncHandlerACType
-type ResetACType = ReturnType<typeof resetAC>
-type MinTitleChangeACType = ReturnType<typeof minTitleChangeAC>
-type MaxTitleChangeACType = ReturnType<typeof maxTitleChangeAC>
-type SetHandlerACType = ReturnType<typeof setHandlerAC>
-type IncHandlerACType = ReturnType<typeof incHandlerAC>
-export const resetAC = () => {
-    return {
-        type: 'RESET'
-    } as const
-}
-export const minTitleChangeAC = (value: string) => {
-    return {
-        type: 'MIN_TITLE',
-        payload: {value}
-    } as const
-}
-export const maxTitleChangeAC = (value: string) => {
-    return {
-        type: 'MAX_TITLE',
-        payload: {value}
-    } as const
-}
-export const setHandlerAC = () => {
-    return {
-        type: 'SET_HANDLER'
-    } as const
-}
-export const incHandlerAC = () => {
-    return {
-        type: 'INC_HANDLER'
-    } as const
-}
+import {incHandlerAC, maxTitleChangeAC, minTitleChangeAC, reducer, resetAC, setHandlerAC, StateType} from "../Redux/Reducer";
 
 function App() {
-
     const storageMinValueAsString = localStorage.getItem('counterMinValue');
     const storageMinValue = storageMinValueAsString ? +storageMinValueAsString : 0;
     const storageMaxValueAsString = localStorage.getItem('counterMaxValue');
@@ -130,14 +36,10 @@ function App() {
         dispatchState(maxTitleChangeAC(e.currentTarget.value));
     }
     const setHandler = () => {
-        dispatchState({
-            type: 'SET_HANDLER'
-        });
+        dispatchState(setHandlerAC());
     }
     const incHandler = () => {
-        dispatchState({
-            type: 'INC_HANDLER',
-        });
+        dispatchState(incHandlerAC());
     }
     const resetHandler = () => {
         localStorage.removeItem('counterValue') // удаление сохраненного значения счетчика
@@ -161,7 +63,6 @@ function App() {
                 </BrowserRouter>
             </div>
         </div>
-
     );
 }
 
